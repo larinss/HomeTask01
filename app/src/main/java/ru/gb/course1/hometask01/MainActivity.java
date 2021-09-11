9b9f96280eaa51e5da6a5544281b303fc1b4a19e
larinss <larinss.new@gmail.com>
2021-09-11 21:57:07 +0300

    Create TextView

--- app/src/main/java/ru/gb/course1/hometask01/MainActivity.java
+++ app/src/main/java/ru/gb/course1/hometask01/MainActivity.java
@@ -3,12 +3,17 @@ package ru.gb.course1.hometask01;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView greetingsTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        greetingsTextView = findViewById(R.id.greetings_text_view);
+        greetingsTextView.setText("Ololo");
     }
 }
