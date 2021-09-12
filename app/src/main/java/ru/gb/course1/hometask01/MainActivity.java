471fc86b7fd11ad04fabfc4b65e98b21f4501bd9
larinss <larinss.new@gmail.com>
2021-09-12 19:46:49 +0300

    Create EditText(логин и пароль) and Button(Войти)

--- app/src/main/java/ru/gb/course1/hometask01/MainActivity.java
+++ app/src/main/java/ru/gb/course1/hometask01/MainActivity.java
@@ -3,10 +3,16 @@ package ru.gb.course1.hometask01;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private TextView greetingsTextView;
+    private Button buttonLogIn;
+    private EditText editTextLogin;
+    private EditText editTextPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +20,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         greetingsTextView = findViewById(R.id.greetings_text_view);
-        greetingsTextView.setText("Ololo");
+        greetingsTextView.setText("Введите логин и пароль");
+
+        buttonLogIn = findViewById(R.id.button_log_in);
+        buttonLogIn.setOnClickListener(view -> {
+
+        });
+        editTextLogin = findViewById(R.id.edit_text_login);
+        editTextPassword = findViewById(R.id.edit_text_password);
     }
 }
