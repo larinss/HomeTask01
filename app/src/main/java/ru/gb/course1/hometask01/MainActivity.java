98eb4c0acee0aa6fdcf4bcf9120b063f8d70fd05
larinss <larinss.new@gmail.com>
2021-09-12 19:57:35 +0300

    Create CheckBoxRemember

--- app/src/main/java/ru/gb/course1/hometask01/MainActivity.java
+++ app/src/main/java/ru/gb/course1/hometask01/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -13,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     private Button buttonLogIn;
     private EditText editTextLogin;
     private EditText editTextPassword;
+    private CheckBox checkBoxRemember;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,11 +25,21 @@ public class MainActivity extends AppCompatActivity {
         greetingsTextView = findViewById(R.id.greetings_text_view);
         greetingsTextView.setText("Введите логин и пароль");
 
-        buttonLogIn = findViewById(R.id.button_log_in);
-        buttonLogIn.setOnClickListener(view -> {
 
-        });
         editTextLogin = findViewById(R.id.edit_text_login);
         editTextPassword = findViewById(R.id.edit_text_password);
+
+        buttonLogIn = findViewById(R.id.button_log_in);
+        buttonLogIn.setOnClickListener(view -> {
+        });
+
+        checkBoxRemember =findViewById(R.id.checkbox_remember);
+        checkBoxRemember.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+
+            }
+        });
+
     }
 }
