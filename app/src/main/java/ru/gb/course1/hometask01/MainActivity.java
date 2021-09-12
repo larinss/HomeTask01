706c2341a30c490b35a2b5bbd0c3ca77cf2a5e94
larinss <larinss.new@gmail.com>
2021-09-12 21:20:22 +0300

    Create Switch and ToggleButton.Different fix.

--- app/src/main/java/ru/gb/course1/hometask01/MainActivity.java
+++ app/src/main/java/ru/gb/course1/hometask01/MainActivity.java
@@ -8,22 +8,26 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView greetingsTextView;
+    private TextView textViewEnterLoginPassword;
     private Button buttonLogIn;
     private EditText editTextLogin;
     private EditText editTextPassword;
     private CheckBox checkBoxRemember;
+    private Switch switchInvisibility;
+    private ToggleButton toggleButtonHelp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        greetingsTextView = findViewById(R.id.greetings_text_view);
-        greetingsTextView.setText("Введите логин и пароль");
+        textViewEnterLoginPassword = findViewById(R.id.text_view_enter_log_pass);
+        textViewEnterLoginPassword.setText("Введите логин и пароль");
 
 
         editTextLogin = findViewById(R.id.edit_text_login);
@@ -33,13 +37,18 @@ public class MainActivity extends AppCompatActivity {
         buttonLogIn.setOnClickListener(view -> {
         });
 
-        checkBoxRemember =findViewById(R.id.checkbox_remember);
+        checkBoxRemember = findViewById(R.id.checkbox_remember);
         checkBoxRemember.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
 
             }
         });
+
+        switchInvisibility = findViewById(R.id.switch_invisibility);
+
+        toggleButtonHelp = findViewById(R.id.toggle_button_help);
+
 
     }
 }
